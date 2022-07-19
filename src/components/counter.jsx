99da6211d65c966c2fa1240b0c270a27a05be52d
morteza <mgahangir2006@gmail.com>
2022-07-19 16:42:50 +0430

    Settings Attributes-2

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,26 +2,25 @@ import React, { Component } from 'react'
 
 class Counter extends Component{
     state = {
-        count: 0, 
-        imageUrl: "https://picsum.photos/201"
-    };
-
-    styles = {
-        fontSize: 18 ,
-        fontWeight: 'bold'
+        count :0
     };
 
     render() {
+        let classes = this.getBadgeClasses();//Refactor 
+  
         return(
             <div>
-                <img  src={this.state.imageUrl} alt=""/>
-                
-                <span style={this.styles} class="badge bg-primary mt-2">{this.formatcount()}</span>
-
-                <button class="btn btn-secondary btn-sm">Increment</button>
+                <span class={classes}>{this.formatcount()}</span>
+                <button class="btn btn-primary btn-sm">Increment</button>
             </div>
         );
     }
+    getBadgeClasses() {
+        let classes = "badge m-2 bg-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+
     formatcount(){
         const { count } = this.state;
         return count === 0  ? "Zero" : count;
