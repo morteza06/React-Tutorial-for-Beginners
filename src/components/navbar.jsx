e7dcf9aef0b6982b9be43d0a8ecb9d38b2db54ff
morteza <mgahangir2006@gmail.com>
2022-07-20 19:16:37 +0430

    Lifting State Up

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,14 +5,20 @@ class NavBar extends Component {
         return( 
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container">
-                    <a className="navbar-brand" href="#">Navbar</a>
+                    <a className="navbar-brand" href="#">
+                        Navbar
+                    </a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        <a className="nav-link active" aria-current="page" href="#">Home 
+                            <span className="badge rounded-pill bg-info text-dark">
+                                {this.props.totalCounters}
+                            </span>
+                        </a>
                         </li>
                         <li className="nav-item">
                         <a className="nav-link" href="#">Features</a>
