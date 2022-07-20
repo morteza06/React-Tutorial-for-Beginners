c07efdc7d876af4d46fe20b17be795f91e576acb
morteza <mgahangir2006@gmail.com>
2022-07-20 11:08:43 +0430

    Composing Component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.css"
-import Counter from './components/counter';
+import Counters from './components/counters';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Counter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Counter>
-);
+ReactDOM.render(<Counters/>, document.getElementById('root'));
 reportWebVitals();
