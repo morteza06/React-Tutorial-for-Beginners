20ae1b174d78e41e5e7ff6931145b22e2a060208
morteza <mgahangir2006@gmail.com>
2022-07-20 19:23:28 +0430

    Stateless Functional Components

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,40 +1,39 @@
 import React, { Component } from 'react';
 
-class NavBar extends Component {
-    render() {
-        return( 
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="container">
-                    <a className="navbar-brand" href="#">
-                        Navbar
+// stateless Functional Component
+const NavBar = () => {
+    return( 
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container">
+                <a className="navbar-brand" href="#">
+                    Navbar
+                </a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Home 
+                        <span className="badge rounded-pill bg-info text-dark">
+                            {this.props.totalCounters}
+                        </span>
                     </a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">Home 
-                            <span className="badge rounded-pill bg-info text-dark">
-                                {this.props.totalCounters}
-                            </span>
-                        </a>
-                        </li>
-                        <li className="nav-item">
-                        <a className="nav-link" href="#">Features</a>
-                        </li>
-                        <li className="nav-item">
-                        <a className="nav-link" href="#">Pricing</a>
-                        </li>
-                        <li className="nav-item">
-                        <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                        </li>
-                    </ul>
-                    </div>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">Features</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
                 </div>
-            </nav>
-        );
-    }
+            </div>
+        </nav>
+    );
 }
 
 export default NavBar;
